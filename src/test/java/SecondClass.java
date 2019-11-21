d58a2a4402479ec814c3944971660dc4658a6a3d
Mizgin1993 <mizginayaz2@gmail.com>
2019-11-21 16:45:50 -0500

    haydi bakalim

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,7 +1,7 @@
 public class SecondClass {
 
     public static void main(String[] args) {
-      String myName = "Homayra";
+      String myName = "Mizgin";
         System.out.println(myName);
     }
 }
