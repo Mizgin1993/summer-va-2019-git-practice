1362e000f91d5386cb186937e164b7cdb35df16d
Mizgin1993 <55104604+Mizgin1993@users.noreply.github.com>
2019-12-05 15:46:27 -0500

    Update SecondClass.java

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,7 +1,7 @@
 public class SecondClass {
 
     public static void main(String[] args) {
-      String myName = "Mizgin";
+      String myName = "Luis Patron";
         System.out.println(myName);
     }
 }
